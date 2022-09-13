f8108bcdbe91f11366be9e9ed9e4a516a9bd49d7
dylangriess <dylangriess@gmail.com>
2022-09-12 20:53:56 -0600

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,82 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+const { restore } = require("../../models/Product");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  try {
+    // find all categories
+    const categoryGet = await Category.findAll({
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryGet);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
+  try {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  const categoryGet = await Category.findByPk(req.params.id, {
+    // be sure to include its associated Products
+    include: [{ model: Product}],
+  }); if (!categoryGet) {
+    res.status(404).json({ message: "Unable to find Category with that ID!" });
+    return;
+  }
+  res.status(200).json(categoryGet);
+} catch (err) {
+  res.status(400).json(err);
+}
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const categoryPost = await Category.create({
+      category_name: req.body.category_name,
+    });
   // create a new category
+    res.status(200).json(categoryPost);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  try{
+    const categoryPut = await Category.update(req.body, {
+      // update a category by its `id` value
+      where: {
+        id: req.params.id,
+      }
+    }); 
+    if (!categoryPut) {
+      res.status(404).json({ message: "Unable to update Category with that ID!" });
+      return;
+    } res.status(200).json(categoryPut);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+router.delete("/:id", async (req, res) => {
+  try {
+    // delete a category by its 'id' value
+    const categoryDelete = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+      if (!categoryDelete) {
+        res.status(404).json({ message: "Unable to delete Category with that ID!" });
+        return;
+      }; res.status(200).json(categoryDelete);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;
